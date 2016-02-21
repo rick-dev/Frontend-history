011647f89c8a932eccb2ba100747de4f5aaf7697
Rikardo Ribeyro <Rikardo.ribeyro@gmail.com>
2016-02-21 02:32:17 +0200

    Add MK-Expert

---
+++ My-work/МК-Эксперт/app/js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+		$("#btn2").hover(function () {
+			$("#btn1").toggleClass("active"); 
+			$("#btn2").toggleClass("active"); 
+		});
+
+	});
+
+	$(document).ready(function () {
+
+		$("#btn3").hover(function () {
+			$("#btn1").toggleClass("active"); 
+			$("#btn3").toggleClass("active"); 
+		});
+
+	});
